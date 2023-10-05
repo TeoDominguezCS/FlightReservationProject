4b22f961d28a73048e829d58ccbfac9535fad327
ricky <rickyramos834@gmail.com>
2023-10-05 09:37:58 -0700

    test commit push

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is the new repo!");
         System.out.println("Changing code on a different computer and git account!");
+        System.out.println("test - ricky");
     }
 }
