c1534b14229ceb05f5a27e00aadc8bc8001f8975
teo <70834248+TeoDominguezCS@users.noreply.github.com>
2023-10-10 14:50:12 -0700

    FlightSearcher Commit
    basic search when given a arrival/departure location. Returns a ResultSet. Basic test of the method has been placed in Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.sql.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("This is the new repo!");
-        System.out.println("Changing code on a different computer and git account!");
-        System.out.println("test - ricky");
-        System.out.println("test - Jonathan");
+    public static void main(String[] args){
+        //testing class/method
+        FlightSearcher a = new FlightSearcher();
+        String departure = "Los Angeles (LAX)";
+        String arrival = "New York (JFK)";
+        try {
+            ResultSet testSet = null;
+            testSet = a.searchAllFlights(arrival, departure);
+            //testing ResultSet returned correctly by printing (didn't print the whole row btw)
+            //EXPECTED: 104 New York (JFK) Los Angeles (LAX)
+            //          105 New York (JFK) Los Angeles (LAX)
+            while (testSet.next()) {
+                System.out.println(testSet.getInt("flightID") + " " + testSet.getString("departureLocation") + " " + testSet.getString("arrivalLocation"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
