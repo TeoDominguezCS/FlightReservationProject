e65f505649f296ddb1c9f2fc9320bc0843a1897a
teo <70834248+TeoDominguezCS@users.noreply.github.com>
2023-10-14 17:25:16 -0700

    Finalizing the FlightSearcher

--- src/FlightSearcher.java
+++ src/FlightSearcher.java
@@ -1,25 +1,47 @@
 import java.sql.*;
 
 public class FlightSearcher {
-    //Returning a ResultSet of flights with atleast 1 open seat given a departure and arrival location
-    public ResultSet searchAllFlights(String departure, String arrival){
+    //Returning a ResultSet of any flight with open seats given a departure and arrival location and number of tickets
+    public ResultSet searchAllFlights(String departure, String arrival, int numTickets) {
+        String sql = "SELECT * FROM flightsTable WHERE departureLocation=? AND arrivalLocation=? AND (currTotalSeats >= ?)";
+        return databaseQuery(sql, departure, arrival, numTickets);
+    }
+    //searching for flights with the specified amount of first class seats given a departure and arrival location
+    public ResultSet searchFirstClassFlights(String departure, String arrival, int numTickets){
+        String sql = "SELECT * FROM flightsTable WHERE departureLocation=? AND arrivalLocation=? AND (currFirstSeats >= ?)";
+        return databaseQuery(sql, departure, arrival, numTickets);
+    }
+    //searching for flights with the specified amount of business class seats given a departure and arrival location
+    public ResultSet searchBusinessClassFlights(String departure, String arrival, int numTickets){
+        String sql = "SELECT * FROM flightsTable WHERE departureLocation=? AND arrivalLocation=? AND (currBusinessSeats >= ?)";
+        return databaseQuery(sql, departure, arrival, numTickets);
+    }
+    //searching for flights with the specified amount of economy class seats given a departure and arrival location
+    public ResultSet searchEconomyClassFlights(String departure, String arrival, int numTickets){
+        String sql = "SELECT * FROM flightsTable WHERE departureLocation=? AND arrivalLocation=? AND (currEconomySeats >= ?)";
+        return databaseQuery(sql, departure, arrival, numTickets);
+    }
+    //does the repeated work of accessing the database and passing the sql through a prepared statement. Returns a resultSet
+    public ResultSet databaseQuery(String sql, String departure, String arrival, int numTickets){
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-        try{
+        try {
             //open a database connection
             Connection connection = DriverManager.getConnection("jdbc:mysql://airlinedatabase.ceof6ckatc9m.us-east-2.rds.amazonaws.com:3306/airlineDatabase", "admin", "!Javathehut23");
             //sql statement to execute with prepared statement
-            String sql = "SELECT * FROM flightsTable WHERE departureLocation=? AND arrivalLocation=? AND (currFirstSeats > 0 OR currBusinessSeats > 0 OR currEconomySeats > 0)";
             preparedStatement = connection.prepareStatement(sql);
             //passing parameters into the sql statement
             preparedStatement.setString(1, departure);
             preparedStatement.setString(2, arrival);
+            preparedStatement.setInt(3, numTickets);
             //executing
             resultSet = preparedStatement.executeQuery();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return resultSet;
     }
 }
+
+
+
